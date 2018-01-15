0bf1e9e5e7d121cda78759775eac57cae8f2125a
Cas van 't Wout <cas@bolboosch.nl>
2018-01-15 17:46:13 +0100

    Created company and landing pages

---
+++ src/app/bedrijf/bedrijf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-landing',
+    templateUrl: './bedrijf.component.html',
+    styleUrls: ['./bedrijf.component.scss']
+})
+
+export class BedrijfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
